561828ad84435151c2467c81e8293f313d93bc32
Ievgen <baranoffei@gmail.com>
2023-03-30 16:35:37 -0400

    Create quizz option component

---
+++ src/app/assessment/container/quiz-options/quiz-options.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'quiz-quiz-options',
+  templateUrl: './quiz-options.component.html',
+  styleUrls: ['./quiz-options.component.scss']
+})
+export class QuizOptionsComponent {
+
+}
