61a1d802c398186f1a25af789f720d3c9c81046c
Ievgen <baranoffei@gmail.com>
2023-03-30 16:22:15 -0400

    Create assesment module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssessmentModule } from './assessment/assessment.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AssessmentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
