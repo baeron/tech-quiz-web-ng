1157ddc199c38c0f262642f8d4dc5fc01abe39fb
Ievgen <baranoffei@gmail.com>
2023-03-30 16:09:06 -0400

    Create boilerplate app

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'quiz-root',
+  template: ` POC for Tech Quiz Web App
+              <router-outlet></router-outlet>
+            `
+})
+export class AppComponent {
+}
